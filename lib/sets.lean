03ba8d14ed2324a00e2f5bdcaff1224b3dc5515b
Etienne Laurin <etienne@atnnn.com>
2018-09-02 13:45:46 +0100

    has_upper_bound seems to work

--- lib/sets.lean
+++ lib/sets.lean
@@ -8,3 +8,23 @@ def all (α : Type u) : set α := λ _ : α, true
 
 def set_sum (s : set ℕ) [fintype s] : ℕ
 := list.sum $ multiset.sort (nat.le) s.to_finset.val
+
+class has_upper_bound {α} [has_le α] (s : set α) :=
+(ub : α) (h : ∀ x : α, s x → x ≤ ub)
+
+instance upper_bound_eq {k : ℕ} : has_upper_bound (λ x, x = k) := ⟨k, by simp⟩
+
+instance uper_bound_le {k : ℕ} : has_upper_bound (λ x, x ≤ k) := ⟨k, by simp⟩
+
+instance uper_bound_lt {k : ℕ} : has_upper_bound (λ x, x < k) := ⟨k, by { intros x h, exact le_of_lt h }⟩
+
+instance uper_bound_and_right {q : Prop} {p : ℕ → Prop} [r : has_upper_bound p] : has_upper_bound (λ x, q ∧ p x) :=
+⟨r.ub, by { intros x h, have rh := r.h, exact rh x h.2}⟩
+
+instance uper_bound_and_left {p : ℕ → Prop} {q : Prop} [l : has_upper_bound p] : has_upper_bound (λ x, p x ∧ q) :=
+⟨l.ub, by { intros x h, have lh := l.h, exact lh x h.1}⟩
+
+instance uper_bound_set_of {α} [has_le α] {p : α → Prop} [hub : has_upper_bound p]: has_upper_bound (set_of p) := hub
+
+example : has_upper_bound.ub ( λ x , true ∧ x ≤ 2) = 2 := rfl
+example : has_upper_bound.ub { x | true ∧ x ≤ 2} = 2 := rfl
--- projecteuler/problem1.lean
+++ projecteuler/problem1.lean
@@ -1,26 +1,9 @@
+import lib.tactics
 import lib.sets
 
-def split_and (
+#print fintype.mk
 
-class has_upper_bound {α} [has_le α] (s : set α) :=
-(ub : α) (h : ∀ x : α, s x → x ≤ ub)
-
-def tmp : has_upper_bound (λ x, x = 1) := ⟨1, by simp⟩
-
-meta def find_nat_ub_tac : tactic unit :=
-  `[exact tmp]
-
-instance find_nat_ub {s : set ℕ} (hub : has_upper_bound s . find_nat_ub_tac) : has_upper_bound s := hub
-
-def tmp2 : has_upper_bound (λ x, x = 1) := find_nat_ub
-
-#reduce tmp2.ub
-
-def foo [hub : has_upper_bound (λ x, x = 1)] : ℕ := hub.ub
-
-#eval foo
-
-instance fintype_bounded_nat {p : set ℕ} [has_upper_bound p] : fintype p := sorry
+instance fintype_bounded_nat {p : set ℕ} [hub : has_upper_bound p] : fintype p := _
 
 def problem : ℕ :=
   set_sum { n | (3 ∣ n ∨ 5 ∣ n) ∧ n < 1000 }
