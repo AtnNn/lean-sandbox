b1bc16b80494d2959e63369c7fc3c7fed305427b
Etienne Laurin <etienne@atnnn.com>
2019-12-28 16:50:02 +0000

    challenge 6

---
+++ src/xena_challenge/challenge06.lean
@@ -0,0 +1,17 @@
+import data.set.basic
+import lib.attempt
+
+open set
+
+def equivalence_class {X : Type} (R : X → X → Prop) (x : X) := {y : X | R x y}
+
+theorem challenge06 (X : Type) (R : X → X → Prop) (hR : equivalence R) (x : X) : equivalence_class R x ≠ ∅ :=
+attempt begin
+  intro h,
+  replace h := congr_fun h x,
+  have r := hR.1 x,
+  replace h : R x x = false := h,
+  rw h at r,
+  exact r
+end $
+λ h, (congr_fun h x).mp $ hR.1 x
